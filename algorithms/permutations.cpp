dae8f55157c22f38795a01fb859606fad2ee6cdc
Samuel <87875402+froge159@users.noreply.github.com>
2023-06-08 23:00:55 -0500

    Update permutations.cpp

--- algorithms/permutations.cpp
+++ algorithms/permutations.cpp
@@ -2,13 +2,13 @@ void search() {
   if (permutation.size() == n) {
   // process permutation
   } else {
-    for (int i = 0; i < n; i++) {
-    if (chosen[i]) continue;
-    chosen[i] = true;
-    permutation.push_back(i);
-    search();
-    chosen[i] = false;
-    permutation.pop_back();
+      for (int i = 0; i < n; i++) {
+        if (chosen[i]) continue;
+        chosen[i] = true;
+        permutation.push_back(i);
+        search();
+        chosen[i] = false;
+        permutation.pop_back();
     }
   }
 }
