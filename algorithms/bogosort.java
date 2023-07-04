8062a238ae87062e3f8128ad2cb4088ceb926d71
froge <87875402+froge159@users.noreply.github.com>
2023-07-03 21:19:50 -0500

    added random counter

--- algorithms/bogosort.java
+++ algorithms/bogosort.java
@@ -13,12 +13,13 @@ public class bogosort {
       for (int i = 0; i < n; i++) {
          arr.add(Integer.parseInt(st.nextToken()));
       }
-
+      int count = 0;
       while(!(arr.stream().sorted().collect(Collectors.toList()).equals(arr))) {
-         Collections.shuffle(arr);
+         Collections.shuffle(arr); count++; pw.println(arr.toString());
       }
-
-      pw.println(arr.toString());
+      pw.println();
+      pw.println("Sorted " + arr.toString());
+      pw.println(count + " randomizations");
       pw.close();
 
 
