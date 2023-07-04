b51e674cc201fb432d3ae26c762df0a1c5e5b94d
froge <87875402+froge159@users.noreply.github.com>
2023-07-03 21:08:56 -0500

    Create bozosort.java

---
+++ algorithms/bozosort.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+import java.util.stream.Collectors;
+
+public class bozosort {
+   public static void main(String[] args) throws IOException {
+      ArrayList<Integer> arr = new ArrayList<Integer>();
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      PrintWriter pw = new PrintWriter(System.out);
+
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      int n = Integer.parseInt(st.nextToken());
+      st = new StringTokenizer(br.readLine());
+      for (int i = 0; i < n; i++) {
+         arr.add(Integer.parseInt(st.nextToken()));
+      }
+      Random rand = new Random();
+      while(!(arr.stream().sorted().collect(Collectors.toList()).equals(arr))) {
+         int x = rand.nextInt(arr.size()); int y = rand.nextInt(arr.size());
+         Collections.swap(arr, x, y);
+         System.out.println(arr.toString());
+      }
+
+      pw.println("Sorted " + arr.toString());
+      pw.close();
+   }
+}
