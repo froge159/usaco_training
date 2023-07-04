b92b027097b1d8a7fcb5c21b4645e7edb01a157e
froge <87875402+froge159@users.noreply.github.com>
2023-07-03 21:08:21 -0500

    Update bogosort.java

--- algorithms/bogosort.java
+++ algorithms/bogosort.java
@@ -3,14 +3,12 @@ import java.io.*;
 import java.util.stream.Collectors;
 
 public class bogosort {
-   static ArrayList<Integer> arr = new ArrayList<Integer>();
-   static int n;
    public static void main(String[] args) throws IOException {
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
       PrintWriter pw = new PrintWriter(System.out);
-
+      ArrayList<Integer> arr = new ArrayList<Integer>();
       StringTokenizer st = new StringTokenizer(br.readLine());
-      n = Integer.parseInt(st.nextToken());
+      int n = Integer.parseInt(st.nextToken());
       st = new StringTokenizer(br.readLine());
       for (int i = 0; i < n; i++) {
          arr.add(Integer.parseInt(st.nextToken()));
