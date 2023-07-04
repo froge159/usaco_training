b79a5e624af356c585d0b76f4ce6cfa7768ba897
froge <87875402+froge159@users.noreply.github.com>
2023-07-03 21:16:58 -0500

    added swap counter

--- algorithms/bozosort.java
+++ algorithms/bozosort.java
@@ -2,12 +2,12 @@ import java.util.*;
 import java.io.*;
 import java.util.stream.Collectors;
 
-public class bozosort {
+public class Testing {
    public static void main(String[] args) throws IOException {
       ArrayList<Integer> arr = new ArrayList<Integer>();
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
       PrintWriter pw = new PrintWriter(System.out);
-
+      int count = 0;
       StringTokenizer st = new StringTokenizer(br.readLine());
       int n = Integer.parseInt(st.nextToken());
       st = new StringTokenizer(br.readLine());
@@ -19,9 +19,11 @@ public class bozosort {
          int x = rand.nextInt(arr.size()); int y = rand.nextInt(arr.size());
          Collections.swap(arr, x, y);
          System.out.println(arr.toString());
+         count++;
       }
 
       pw.println("Sorted " + arr.toString());
+      pw.println(count + " swaps");
       pw.close();
    }
 }
