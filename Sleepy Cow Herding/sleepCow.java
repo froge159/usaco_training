34f297ea05447039892bfe80a91350ffcf3f6e18
froge <87875402+froge159@users.noreply.github.com>
2023-08-09 10:55:46 -0500

    Update sleepCow.java

--- Sleepy Cow Herding/sleepCow.java
+++ Sleepy Cow Herding/sleepCow.java
@@ -13,17 +13,30 @@ public class sleepCow {
       int[] arr = new int[3];
       int min = 0; int max = 0;
       StringTokenizer st = new StringTokenizer(br.readLine());
-      for (int i = 0; i < 3; i++) {arr[i] = Integer.parseInt(st.nextToken());}
+      for (int i = 0; i < 3; i++) {
+          arr[i] = Integer.parseInt(st.nextToken());
+      }
       Arrays.sort(arr);
       int[] ar = arr.clone();
-      ArrayList<Integer> ranges = new ArrayList<>(); ranges.add(arr[1]-arr[0]); ranges.add(arr[2] - arr[1]);
+      ArrayList<Integer> ranges = new ArrayList<>(); 
+      ranges.add(arr[1]-arr[0]); 
+      ranges.add(arr[2] - arr[1]);
       Collections.sort(ranges);
-      if (ranges.get(0) == 1) {min = check(1, ranges, min);}
-      else {min = check(0, ranges, min);}
-
-      int[] ranges2 = {ar[1] - ar[0], ar[2] - ar[1]}; Arrays.sort(ranges2); int maxRange = ranges2[1];
-      if (maxRange == ar[1] - ar[0]) {max = ar[1]- ar[0];}
-      else if (maxRange == ar[2] - ar[1]) {max = ar[2] - ar[1];}
+      if (ranges.get(0) == 1) {
+          min = check(1, ranges, min);
+      }
+      else {
+          min = check(0, ranges, min);
+      }
+      int[] ranges2 = {ar[1] - ar[0], ar[2] - ar[1]}; 
+      Arrays.sort(ranges2); 
+      int maxRange = ranges2[1];
+      if (maxRange == ar[1] - ar[0]) {
+          max = ar[1]- ar[0];
+      }
+      else if (maxRange == ar[2] - ar[1]) {
+          max = ar[2] - ar[1];
+      }
       pw.println(min);pw.println(max -1);
       pw.close();
    }
