3c13323d19a859ce9b8bcf4ef5886208ddbc556a
froge <87875402+froge159@users.noreply.github.com>
2023-09-01 10:07:33 -0500

    formatted loop

--- Sleepy Cow Sorting/sleepy.java
+++ Sleepy Cow Sorting/sleepy.java
@@ -9,7 +9,9 @@ public class sleepy {
       int[] arr = new int[n];
       int count = 0;
       StringTokenizer st = new StringTokenizer(br.readLine());
-      for (int i = 0; i < n; i++) {arr[i] = Integer.parseInt(st.nextToken());}
+      for (int i = 0; i < n; i++) {
+         arr[i] = Integer.parseInt(st.nextToken());
+      }
 
       for (int i = n - 1; i > 0; i--) {
          if (arr[i] > arr[i - 1]) {count++;}
