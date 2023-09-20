aeb9121e14fe381f3e32f91f166bf1741db70d28
froge <87875402+froge159@users.noreply.github.com>
2023-09-20 14:32:32 -0500

    Update temporaryFile.java

--- temporaryFile.java
+++ temporaryFile.java
@@ -1,92 +1,35 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
-public class temporaryFile {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int T = scanner.nextInt(); // Number of test cases
-        
-        for (int t = 0; t < T; t++) {
-            scanner.nextLine(); // Read and discard the empty line
-            
-            int N = scanner.nextInt(); // Size of the grid
-            scanner.nextLine(); // Move to the next line
-            
-            char[][] grid = new char[N][N];
-            for (int i = 0; i < N; i++) {
-                String line = scanner.nextLine();
-                grid[i] = line.toCharArray();
-            }
-            
-            int K = scanner.nextInt(); // Size of the stamp
-            scanner.nextLine(); // Move to the next line
-            
-            char[][] stamp = new char[K][K];
-            for (int i = 0; i < K; i++) {
-                String line = scanner.nextLine();
-                stamp[i] = line.toCharArray();
-            }
-            
-            char[][] ans = new char[N][N];
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                    ans[i][j] = '.';
-                }
-            }
-            
-            for (int rot = 0; rot < 4; rot++) {
-                for (int i = 0; i <= N - K; i++) {
-                    for (int j = 0; j <= N - K; j++) {
-                        boolean canStamp = true;
-                        for (int a = 0; a < K; a++) {
-                            for (int b = 0; b < K; b++) {
-                                if (grid[i + a][j + b] == '*' && stamp[a][b] == '.') {
-                                    canStamp = false;
-                                    break;
-                                }
-                            }
-                            if (!canStamp) {
-                                break;
-                            }
-                        }
-                        
-                        if (canStamp) {
-                            for (int a = 0; a < K; a++) {
-                                for (int b = 0; b < K; b++) {
-                                    if (stamp[a][b] == '*') {
-                                        ans[i + a][j + b] = '*';
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-                
-                // Rotate the stamp 90 degrees clockwise
-                char[][] newStamp = new char[K][K];
-                for (int a = 0; a < K; a++) {
-                    for (int b = 0; b < K; b++) {
-                        newStamp[a][b] = stamp[K - 1 - b][a];
-                    }
-                }
-                stamp = newStamp;
-            }
-            
-            boolean isEqual = true;
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                    if (grid[i][j] != ans[i][j]) {
-                        isEqual = false;
-                        break;
-                    }
-                }
-                if (!isEqual) {
-                    break;
-                }
-            }
-            
-            System.out.println(isEqual ? "YES" : "NO");
-        }
-        
-        scanner.close();
-    }
+class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		PrintWriter pw = new PrintWriter(System.out);
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		int[] arr = new int[N + 1];
+		int[][] ranges = new int[2][2];
+
+		for (int i = 0; i < 2; i++) {
+			st = new StringTokenizer(br.readLine());
+			ranges[i][0] = Integer.parseInt(st.nextToken());
+			ranges[i][1] = Integer.parseInt(st.nextToken());
+		}
+		for (int i = 0; i < K; i++) {
+			int[] clone = new int[N + 1];
+			for (int j = 1; j <= N; j++) {
+				int index = j;
+				int orig = j;
+				if (ranges[0][0] <= j && j <= ranges[0][1]) {
+					clone[ranges[0][0] + (ranges[0][1]-j)] = arr[j];
+					index = ranges[0][0] + (ranges[0][1]-j);
+				}
+				if (ranges[1][0] <= index && index <= ranges[1][1]) {
+					clone[ranges[1][0] + (ranges[1][1] - index)] = arr[orig];
+				}
+			}
+			arr = clone;
+		}
+	}
 }
