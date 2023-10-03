113433b402c8b4e80be9d21a0d96549a5c0fd618
froge <87875402+froge159@users.noreply.github.com>
2023-10-02 22:22:10 -0500

    Create BuyAndSellStock.java

---
+++ Momentum Learning/BuyAndSellStock.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import java.io.*;
+
+public class BuyAndSellStock {
+   public static void main(String[] args) throws IOException {
+         // BufferedReader br = new BufferedReader(new FileReader("input.in"));
+         // PrintWriter pw = new PrintWriter("output.out");
+         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+         PrintWriter pw = new PrintWriter(System.out);
+         StringTokenizer st = new StringTokenizer(br.readLine());
+         int N = Integer.parseInt(st.nextToken());
+         st = new StringTokenizer(br.readLine());
+         int[] nums = new int[N];
+         boolean owned = false;
+         int total = 0;
+         for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+         }
+
+         for (int i = 0; i < N; i++) {
+            if (i == 0) {
+               if (total - nums[i] + nums[i + 1] > 0){
+                  total -= nums[i]; 
+                  owned = true;
+               }
+            }
+            else if (i == N - 1) {
+               if (owned) total -= nums[i];
+               break;
+            }
+
+            else if (owned) {
+               total += nums[i];
+               owned = false;
+            }
+
+            else if (nums[i + 1] > total - nums[i]) {
+               total -= nums[i];
+               owned = true;
+            }
+         }
+         pw.println(total);
+         pw.close();
+
+   }
+}
