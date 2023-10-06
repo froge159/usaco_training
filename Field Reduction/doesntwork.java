799825121d26fc64ebc76ebd143eb2cc03a11fea
froge <87875402+froge159@users.noreply.github.com>
2023-10-06 18:53:36 -0500

    Update doesntwork.java

--- Field Reduction/doesntwork.java
+++ Field Reduction/doesntwork.java
@@ -1,106 +1,103 @@
 import java.util.*;
 import java.io.*;
 
-public class doesntwork {
-	static ArrayList<ArrayList<Integer>> p;
-	static int area = 0;
-	static ArrayList<Integer> x;
-	static ArrayList<Integer> y;
-	static int indexes[];
-	public static int search(ArrayList<Integer> arr, int x) {
-        int left = 0, right = arr.size() - 1;
-       
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (arr.get(mid) == x)
-                return mid;
-            if (arr.get(mid) < x)
-                left = mid + 1;
-            else
-                right = mid - 1;
-        }
-        return -1;
-    } // 
-	public static int area(List<Integer> xc, List<Integer> yc) {
-		return Math.abs(xc.get(xc.size()-1)-xc.get(0)) * Math.abs(yc.get(yc.size()-1)-yc.get(0));
+public class FieldReduction {
+	static int N;
+	static int area(ArrayList<Integer> x, ArrayList<Integer> y, int startx, int endx, int starty, int endy) { // takes sorted list
+		return (x.get(startx) - x.get(endx)) * (y.get(starty) - y.get(endy));
 	}
-	public static void remove(ArrayList<Integer> x, ArrayList<Integer> y, int[] indexes, ArrayList<Integer> set) { // removes generated subsets elements
-		ArrayList<Integer> xc = new ArrayList<>(x);
-		ArrayList<Integer> yc = new ArrayList<>(y);
-		for (int i = 0; i < set.size(); i++) {
-			try {
-				xc.remove(search(xc, p.get(set.get(i)).get(0)));
-			}
-			catch (Exception e) {
-				try {
-					yc.remove(search(yc, p.get(set.get(i)).get(1)));
-					continue;
-				}
-				catch (Exception f) {continue;}
-			}
-			try {yc.remove(search(yc, p.get(set.get(i)).get(1)));}
-			catch (Exception idkwhattonamethis) {continue;}
+	static int[] onBorder(ArrayList<Integer> xc, ArrayList<Integer> yc, ArrayList<Integer> pt) {
+		int[] points = new int[4];
+		if (pt.get(0) == xc.get(0) && yc.get(0) <= pt.get(1) && yc.get(N - 1) >= pt.get(1)) {
+         points[0] = 1;
 		}
-		if (area(xc, yc) < area) {area = area(xc, yc);}
-	}
-	public static void powerSet(int[] indexes, int index, ArrayList<Integer> curr) {
-        int n = indexes.length;
- 
-        // base case
-        if (index == n) {
-            return;
-        }
- 
-        // First print current subset
-        if (curr.size() < 4 && curr.size() > 0) {System.out.println(curr.toString());}; 
- 
-        // Try appending remaining characters
-        // to current subset
-        for (int i = index + 1; i < n; i++) {
-			if (curr.size() < 3) {  // Modify this condition to allow subsets of size less than 4
-				curr.add(indexes[i]);
-				powerSet(indexes, i, curr);
-				curr.remove(curr.size() - 1);  // Backtrack by removing the last added element
-			}
+      if (pt.get(0) == xc.get(N - 1) && yc.get(0) <= pt.get(1) && pt.get(1) <= yc.get(N - 1)) {
+         points[1] = 2;
 		}
+      if (pt.get(1) == yc.get(0) && xc.get(0) <= pt.get(0) && pt.get(0) <= xc.get(N - 1)) {
+         points[2] = 3;
+		}
+      if (pt.get(1) == yc.get(N - 1) && xc.get(0) <= pt.get(0) && pt.get(0) <= xc.get(N - 1)) {
+         points[3] = 4;
+		}
+      return points;
 	}
 	public static void main(String[] args) throws IOException {
-		//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		//PrintWriter pw = new PrintWriter(System.out);
-		BufferedReader br = new BufferedReader(new FileReader("reduce.in"));
-		PrintWriter pw = new PrintWriter("reduce.out");
-		// 
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		x = new ArrayList<>(N);
-		y = new ArrayList<>(N);
-		int[] xo = new int[N];
-		int[] yo = new int[N];
-		p = new ArrayList<>(N);
-		ArrayList<Integer> set = new ArrayList<>();
-		for (int i = 0; i < N; i++) {
-			p.add(new ArrayList<Integer>());
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken()); int b = Integer.parseInt(st.nextToken());
-			x.add(a); y.add(b); xo[i] = a; yo[i] = b;
-			p.get(i).add(a); p.get(i).add(b);
+        //BufferedReader br = new BufferedReader(new FileReader("reduce.in"));
+        //PrintWriter pw = new PrintWriter("reduce.out");
+         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+         PrintWriter pw = new PrintWriter(System.out);
+         StringTokenizer st = new StringTokenizer(br.readLine());
+         N = Integer.parseInt(st.nextToken());
+         ArrayList<ArrayList<Integer>> pts = new ArrayList<>(); // all points
+         ArrayList<Integer> xc = new ArrayList<>(); // ordered x points
+         ArrayList<Integer> yc = new ArrayList<>(); // ordered y points
+         for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            pts.add(new ArrayList<Integer>());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            pts.get(i).add(a);  xc.add(a);
+            pts.get(i).add(b); yc.add(b);
+         }
+         Collections.sort(xc); Collections.sort(yc);
+		 // 1 4 7 8 10 50
+		 // 1 7 8 9 12 100
+		ArrayList<Integer> removed = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+			int maxArea = Integer.MIN_VALUE;
+			int index = 0;
+            for (int j = 0; j < pts.size(); j++) {
+				if (removed.contains(j)) { continue; }
+				int startx = N - 1; int endx = 0;
+		 		int starty = N - 1; int endy = 0;
+                int[] faces = onBorder(xc, yc, pts.get(j));
+                // left has min x, right has max x
+                // bottom has min y, top has max y
+				// 1 4 7 8 10 50
+				// 1 7 8 9 12 100
+				int[] clone = faces.clone();
+				Arrays.fill(clone, 0);
+				if (!Arrays.equals(clone, faces)) {
+					for (int z: faces) {
+						if (z == 1) {
+							endx++;
+						}
+						if (z == 2) {
+							startx--;
+						}
+						if (z == 3) {
+							endy++;
+						}
+						if (z == 4) {
+							starty--;
+						}
+					}
+					int area = area(xc, yc, startx, endx, starty, endy);
+					//pw.println(area + " " + j);
+					if (maxArea < area) {maxArea = area; index = j; }
+				}
+			}
+			removed.add(index);
+        }
+		int smallx = Integer.MAX_VALUE; int smally = Integer.MAX_VALUE;
+		int largex = Integer.MIN_VALUE; int largey = Integer.MIN_VALUE;
+		for (int i = 0; i < pts.size(); i++) {
+			if (i == removed.get(0) || i == removed.get(1) || i == removed.get(2)) {
+				pts.get(i).clear();
+			}
+			else {
+				if (pts.get(i).get(0) < smallx) smallx = pts.get(i).get(0);
+				if (pts.get(i).get(0) > largex) largex = pts.get(i).get(0);
+				if (pts.get(i).get(1) < smally) smally = pts.get(i).get(1);
+				if (pts.get(i).get(1) > largey) largey = pts.get(i).get(1);
+			}
+
 		}
-		Collections.sort(x); Collections.sort(y);
-		// get all small and large values  indexing by searching through original x and y array
-		// generate all subsets and find the smallest areas
-		area = Math.abs(x.get(N-1)-x.get(0)) * Math.abs(y.get(N-1)-y.get(0));
-		int smallx = x.get(0); int largex = x.get(N-1);
-		int smally = y.get(0); int largey = y.get(N-1);
-		indexes = new int[4];
-		for (int i = 0; i < N; i++) { 
-			if (smallx == xo[i]) {indexes[0] = i;}
-			if (largex == xo[i]) {indexes[1] = i;}
-			if (smally == yo[i]) {indexes[2] = i;}
-			if (largey == yo[i]) {indexes[3] = i;}
-		} 
-		powerSet(indexes, -1, set);
-		
-		pw.println(area);
+		//pw.println(removed.toString());
+		pw.println((largex - smallx) * (largey - smally));
 		pw.close();
-	}
+         
+
+    }    
 }
