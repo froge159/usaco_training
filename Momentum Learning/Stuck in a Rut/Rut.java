c3dc568a4d9144b4e1ae67d5f900e8a728f3e49d
froge <87875402+froge159@users.noreply.github.com>
2023-10-18 22:10:24 -0500

    Update Rut.java

--- Momentum Learning/Stuck in a Rut/Rut.java
+++ Momentum Learning/Stuck in a Rut/Rut.java
@@ -12,3 +12,27 @@ public class Rut {
         pw.close();
     }
 }
+/*
+Boolean 10001 array holding grass true/false
+Integer 10001 arraylist holding all cows index in specs
+2d arraylist holding all cows specs
+2d array holding ha scow stopped moving
+Array holding each count
+
+
+For I in range 10000
+	If stopped = true, continue;
+	
+	Make changes (make sure to update main grid and specs)
+	If it moved onto an empty cell, stopped = true
+	If there already is a cow on the position a cow is trying to move:
+		Add to that subarray
+	Count++
+	
+	For every cow:
+		Make their corresponding cells empty
+		
+
+Looping through specs:
+	If stopped = true, print out ending count
+Else, print infinity*/
