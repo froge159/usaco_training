fe5ae97ed705e11cd452754154062b5327853bb4
froge <87875402+froge159@users.noreply.github.com>
2023-10-18 21:59:53 -0500

    Create Rut.java

---
+++ Momentum Learning/Stuck in a Rut/Rut.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+public class Rut {
+    public static void main(String[] args) throws IOException {
+        // BufferedReader br = new BufferedReader(new FileReader("input.in"));
+        // PrintWriter pw = new PrintWriter("output.out");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter(System.out);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        pw.close();
+    }
+}
