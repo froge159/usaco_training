5089aff0c102b2569d5ccc31d97ba7bab742512f
froge <87875402+froge159@users.noreply.github.com>
2023-10-19 14:33:26 -0500

    Update temporaryFile.java

--- temporaryFile.java
+++ temporaryFile.java
@@ -1,75 +1,58 @@
-import java.util.*;
-import java.io.*;
+import static java.lang.System.*;
+import java.util.Scanner;
 
-class Make24 {
-    static int count = 0;
-    static void solve(int[] nums, int res) {
-        int n = nums.length;
-        int[] p = new int[n];
-        int i = 1;
-        while (i < n) {
-            if (p[i] < i) {
-                int j = ((i % 2) == 0) ? 0 : p[i];
-                swap(nums, i, j);
-                if (loop(nums, res))  {
-                    count++;
-                    return;
-                }
-                p[i]++;
-                i = 1;
-            }
-            else {
-                p[i] = 0;
-                i++;
-            }
-        }
-    }
-    static void swap(int[] nums, int a, int b) {
-        int temp = nums[a];
-        nums[a] = nums[b];
-        nums[b] = temp;
-    } 
-    static boolean loop(int[] nums, int res) {
-        // + - * /
-        double result = 0;
-        boolean matched = false;
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                for (int k = 0; k < 4; k++) {
-                    result = nums[0];
-                    result = operate(nums, i, result, 1);
-                    result = operate(nums, j, result, 2);
-                    result = operate(nums, k, result, 3);
-                    if (result == res) {
-                        matched = true; 
-                    }
-                }
-            }
-        }
-        return matched;
-    }
-    static double operate(int[] nums, int index, double result, int currIndex) {
-        if (index == 0) return result + nums[currIndex];
-        if (index == 1) return result - nums[currIndex];
-        if (index == 2) return result * nums[currIndex];
-        else return result / nums[currIndex];
-    }
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter pw = new PrintWriter(System.out);
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
-            int d = Integer.parseInt(st.nextToken());
-            int res = Integer.parseInt(st.nextToken());
-            int[] nums = {a, b, c, d};
-            solve(nums, res);
-        }
-        pw.println(count);
-        pw.close();
-    }
+
+public class ClickBait {
+	public static void main(String args[]) 
+	{
+		Scanner input = new Scanner(in);
+		
+		int circleX, circleY, circleWidth;
+		int upperX, upperY, rectangleWidth, rectangleLength;
+		int x,y;
+		boolean isCircle, isRectangle;
+		
+		out.println("Enter in x-coordinate of circle:");
+		circleX = input.nextInt();
+		out.println("Enter in y-coordinate of circle:");
+		circleY = input.nextInt();
+		out.println("Enter in width of circle:");
+		circleWidth = input.nextInt();
+		out.println("Enter in x-coordinate of rectangle:");
+		upperX = input.nextInt();
+		out.println("Enter in y-coordinate of rectangle:");
+		upperY = input.nextInt();
+		out.println("Enter in width of rectangle:");
+		rectangleWidth = input.nextInt();
+		out.println("Enter in height of rectangle:");
+		rectangleLength = input.nextInt();
+		out.println("Enter x-coordinate of mouse click:");
+		x = input.nextInt();
+		out.println("Enter y-coordinate of mouse click:");
+		y = input.nextInt();
+		isCircle = checkCircle(circleX, circleY, circleWidth, x, y);
+		isRectangle = checkRect(upperX, upperY, rectangleWidth, rectangleLength, x, y);
+		printResult(isCircle, isRectangle);
+	}
+	static boolean checkCircle(int circleX, int circleY, int circleWidth, int x, int y) {
+	    double r = circleWidth / 2;
+	    double xp = (circleX + circleWidth) / 2; double yp = (circleY - circleWidth) / 2;
+	    if (Math.sqrt(Math.pow(Math.abs(xp - x), 2) + Math.pow(Math.abs(yp - y), 2)) <= r) {
+	        return true;
+	    }
+	    return false;
+	}
+	static boolean checkRect(int upperX, int upperY, int rectangleWidth,int rectangleLength, int x, int y) {
+	    if (upperX <= x && x <= upperX + rectangleWidth && upperY - rectangleLength <= y && y <= upperY) {
+	        return true;
+	    }
+	    return false;
+	}
+	static void printResult(boolean isCircle, boolean isRectangle) {
+	    if (isCircle && !isRectangle) System.out.println("circle");
+	    else if (!isCircle && isRectangle) System.out.println("rectangle");
+	    else if (!isCircle && !isRectangle) System.out.println("neither");
+	}
+	
+	
 }
