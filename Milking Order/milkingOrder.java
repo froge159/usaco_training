841c5629b6cbac7b9bca70521b1a5f80fcb6b5cc
froge <87875402+froge159@users.noreply.github.com>
2023-11-02 14:32:33 -0500

    Update milkingOrder.java

--- Milking Order/milkingOrder.java
+++ Milking Order/milkingOrder.java
@@ -1,84 +1,53 @@
+/* Obervations:
+first set unmovable cows into respective positions
+we can try putting 1 at every position and check if it satisfies the hierarchy
+
+Solution:
+create empty array of size N
+fill array with unmovable cows
+if 1 is an unmovable cow, return 1 index
+
+for every position available to 1:
+  loop through array again put cows in hierarchy in earliest possible positions. if it is already in array, let it be.
+  loop through array again and check if it satisfies the hierarchy: return index of 1 */
 import java.util.*;
 import java.io.*;
 
-public class milkingOrder {
-   private static boolean containsElement(int[] array, int element) {
-    for (int value : array) {
-        if (value == element) {
+public class Main {
+    static int contains(int[] arr, int num) {
+        Arrays.sort(arr);
+        if (Arrays.binarySearch(arr, num) < 0) {
             return true;
         }
+        return false;
     }
-    return false;
-   }
-   private static int findIndex(int[] array, int element) {
-    for (int i = 0; i < array.length; i++) {
-        if (array[i] == element) {
-            return i;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter(System.out);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[] nums = new int[N + 1];
+        int[] order = new int[M];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) order[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < K; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            if (a == 1) {pw.println(b); pw.close();}
+            nums[b] = a;
         }
+        for (int i = 1; i < N + 1; i++) {
+            if (nums[i] == 0) {
+                int[] clone = nums.clone();
+                clone[i] = 1;
+                for (int j = 0; j < M; j++) {
+                    if (contains(clone, order[j])) 
+                }
+            }
+        }
+        pw.close();
     }
-    return -1;
-}
-   public static void main(String[] args) throws IOException {
-      BufferedReader br = new BufferedReader(new FileReader("milkorder.in"));
-      PrintWriter pw = new PrintWriter("milkorder.out");
-      StringTokenizer st = new StringTokenizer(br.readLine());
-      int n  = Integer.parseInt(st.nextToken()); int m = Integer.parseInt(st.nextToken()); 
-      int k = Integer.parseInt(st.nextToken());
-      int[] x = new int[m];
-      int[] order = new int[n];
-      st = new StringTokenizer(br.readLine());
-      for (int i = 0; i < m; i++) {
-         x[i] = Integer.parseInt(st.nextToken());
-      }
-      for (int i = 0; i < k; i++) {
-         st = new StringTokenizer(br.readLine());
-         int a = Integer.parseInt(st.nextToken());
-         order[Integer.parseInt(st.nextToken()) - 1] = a;
-      }
-      if (containsElement(x, 1)) {
-         for (int i = 0; i <= n - x.length; i++) {
-            int[] clone = order.clone();
-            int index = 0;
-            for (int j = i; j < n; j++) {
-               if (index >= m) {break;}
-               else if (containsElement(x, clone[j])) {index++;}
-               if (clone[j] == 0) { clone[j] = x[index]; index++; }
-            } 
-            ArrayList<Integer> indexes = new ArrayList<>();
-            for (int l = 0; l < m; l++) {
-               indexes.add(findIndex(clone, x[l]));
-            }
-            List<Integer> copy = new ArrayList<>(indexes); Collections.sort(copy);
-            if (copy.equals(indexes)) {pw.println(findIndex(clone, 1) + 1); break;}
-            copy.clear(); indexes.clear();
-         }
-      }
-
-
-
-      else {
-         for (int i = 0; i < n; i++) {
-            int[] clone = order.clone();
-            if (clone[i] == 0) {
-               clone[i] = 1; 
-               int index = 0;    
-               for (int j = 0; j < n; j++) { 
-                  if (index >= m) {break;}
-                  else if (containsElement(clone, x[index])) {index++;}
-                  if (clone[j] == 0) {
-                     clone[j] = x[index]; index++;
-                  }
-               }
-               ArrayList<Integer> indexes = new ArrayList<>();
-               for (int l = 0; l < m; l++) {
-                  indexes.add(findIndex(clone, x[l]));
-               }
-               List<Integer> copy = new ArrayList<>(indexes); Collections.sort(copy);
-               if (copy.equals(indexes)) {pw.println(i + 1); break;}
-               copy.clear(); indexes.clear();
-            }
-         }
-      }
-      pw.close();
-   }
 }
