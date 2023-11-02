5415e2df956deb780b733d4ecc8638c83ebc475f
froge <87875402+froge159@users.noreply.github.com>
2023-11-02 08:10:57 -0500

    Update temporaryFile.java

--- temporaryFile.java
+++ temporaryFile.java
@@ -1,58 +1,30 @@
-import static java.lang.System.*;
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
-
-public class ClickBait {
-	public static void main(String args[]) 
-	{
-		Scanner input = new Scanner(in);
-		
-		int circleX, circleY, circleWidth;
-		int upperX, upperY, rectangleWidth, rectangleLength;
-		int x,y;
-		boolean isCircle, isRectangle;
-		
-		out.println("Enter in x-coordinate of circle:");
-		circleX = input.nextInt();
-		out.println("Enter in y-coordinate of circle:");
-		circleY = input.nextInt();
-		out.println("Enter in width of circle:");
-		circleWidth = input.nextInt();
-		out.println("Enter in x-coordinate of rectangle:");
-		upperX = input.nextInt();
-		out.println("Enter in y-coordinate of rectangle:");
-		upperY = input.nextInt();
-		out.println("Enter in width of rectangle:");
-		rectangleWidth = input.nextInt();
-		out.println("Enter in height of rectangle:");
-		rectangleLength = input.nextInt();
-		out.println("Enter x-coordinate of mouse click:");
-		x = input.nextInt();
-		out.println("Enter y-coordinate of mouse click:");
-		y = input.nextInt();
-		isCircle = checkCircle(circleX, circleY, circleWidth, x, y);
-		isRectangle = checkRect(upperX, upperY, rectangleWidth, rectangleLength, x, y);
-		printResult(isCircle, isRectangle);
-	}
-	static boolean checkCircle(int circleX, int circleY, int circleWidth, int x, int y) {
-	    double r = circleWidth / 2;
-	    double xp = (circleX + circleWidth) / 2; double yp = (circleY - circleWidth) / 2;
-	    if (Math.sqrt(Math.pow(Math.abs(xp - x), 2) + Math.pow(Math.abs(yp - y), 2)) <= r) {
-	        return true;
-	    }
-	    return false;
-	}
-	static boolean checkRect(int upperX, int upperY, int rectangleWidth,int rectangleLength, int x, int y) {
-	    if (upperX <= x && x <= upperX + rectangleWidth && upperY - rectangleLength <= y && y <= upperY) {
-	        return true;
-	    }
-	    return false;
-	}
-	static void printResult(boolean isCircle, boolean isRectangle) {
-	    if (isCircle && !isRectangle) System.out.println("circle");
-	    else if (!isCircle && isRectangle) System.out.println("rectangle");
-	    else if (!isCircle && !isRectangle) System.out.println("neither");
-	}
-	
-	
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // BufferedReader br = new BufferedReader(new FileReader("input.in"));
+        // PrintWriter pw = new PrintWriter("output.out");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter(System.out);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int[] rule = new int[N]; // changes
+        int[] orig = new int[N]; // test
+        for (int i = 0; i < N; i++) {
+            rule[i] = Integer.parseInt(st.nextToken());
+            orig[i] = i + 1;
+        }
+        int[] test = Arrays.clone(orig);
+        for (int i = 0; i < K; i++) {
+            int[] newArr = new int[N];
+            for (int j = 0; j < N; j++) {
+                newArr[rule[j]] = test[j];
+            }
+            test = Arrays.clone(newArr);
+        }
+        pw.close();
+    }
 }
