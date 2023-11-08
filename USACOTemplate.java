3cc3a14886ce8fd169429d9f00f84412be922696
froge <87875402+froge159@users.noreply.github.com>
2023-11-08 08:02:32 -0600

    Update USACOTemplate.java

--- USACOTemplate.java
+++ USACOTemplate.java
@@ -1,16 +1,13 @@
 import java.util.*;
 import java.io.*;
-static BufferedReader br;
-static PrintWriter pw;
-static StringTokenizer st;
 
 public class USACOTemplate {
     public static void main(String[] args) throws IOException {
         // BufferedReader br = new BufferedReader(new FileReader("input.in"));
         // PrintWriter pw = new PrintWriter("output.out");
-        br = new BufferedReader(new InputStreamReader(System.in));
-        pw = new PrintWriter(System.out);
-        st = new StringTokenizer(br.readLine());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter(System.out);
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
         pw.close();
     }
