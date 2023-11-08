fa705c67c5627c148398a8659ce64f8677d32278
froge <87875402+froge159@users.noreply.github.com>
2023-11-08 08:01:40 -0600

    Create Reverse.java

---
+++ December Bronze 2023/Reverse Engineering/Reverse.java
@@ -0,0 +1,15 @@
+/*
+create array containing subarray[conditional, answer, return value]
+
+iterating through all input/outputs:
+       iterate through every possible conditional value:
+             iterate through previous i/o
+                   if does not satisfy, try new conditional value
+             if satisfies all previous i/o, continue
+        if iterated through every possible value, print LIE
+
+        
+find new conditional value:
+    for every index:
+         if val == x return y
+*/
