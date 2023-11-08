525c9d87b5c132ef477a00f2db17b21ac6174745
froge <87875402+froge159@users.noreply.github.com>
2023-11-08 08:08:28 -0600

    Update Reverse.java

--- December Bronze 2023/Reverse Engineering/Reverse.java
+++ December Bronze 2023/Reverse Engineering/Reverse.java
@@ -13,3 +13,33 @@ find new conditional value:
     for every index:
          if val == x return y
 */
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static void solve(PrintWriter pw, String[][] arr) {
+
+    }
+    public static void main(String[] args) throws IOException {
+        // BufferedReader br = new BufferedReader(new FileReader("input.in"));
+        // PrintWriter pw = new PrintWriter("output.out");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter(System.out);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int T = Integer.parseInt(st.nexToken());
+        for (int i = 0; i < T; i++) {
+            st = new StringTokenizer(br.readLine());
+            st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+            String[][] arr = new String[M][2];
+            for (int j = 0; j < M; j++) {
+                st = new StringTokenizer(br.readLine());
+                arr[j][0] = st.nextToken();
+                arr[j][1] = st.nextToken();
+            }
+            solve(pw, arr);
+        }
+        pw.close();
+    }
+}
