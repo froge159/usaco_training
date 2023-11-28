27785b07b0116f179a33222a886d8c1fbb70ea3c
froge <87875402+froge159@users.noreply.github.com>
2023-11-28 08:11:29 -0600

    Update temporaryFile.java

--- temporaryFile.java
+++ temporaryFile.java
@@ -2,45 +2,43 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static void solve(PrintWriter pw, int[] nums) {
-        int count = 0;
-        for (int i = 0; i < nums.length; i++) {
-            int target = 0;
-            for (int j = 0; j <= i; j++) {
-                if (target > 0) count++;
-                target += nums[j];
-            }
-            int curr = 0;
-            for (int j = i + 1; j < nums.length; j++) {
-                if (curr > 0) count++;
-                curr += nums[j];
-                if (curr == target) {
-                    curr = 0;
-                }
-                else if (curr > target) {
-                    count = 0; break;
-                }
-            }
-            if (count > 0) { pw.println(count); return;}
-        }
-    }
     public static void main(String[] args) throws IOException {
         // BufferedReader br = new BufferedReader(new FileReader("input.in"));
         // PrintWriter pw = new PrintWriter("output.out");
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         PrintWriter pw = new PrintWriter(System.out);
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int T = Integer.parseInt(st.nextToken());
-        for (int i = 0; i < T; i++) {
-            st = new StringTokenizer(br.readLine());
-            int N = Integer.parseInt(st.nextToken());
-            st = new StringTokenizer(br.readLine());
-            int[] nums = new int[N];
-            for (int j = 0; j < N; j++) {
-                nums[j] = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        char[] arr = st.nextToken().toCharArray();
+        int count = 0;
+
+        for (int i = 0; i < N; i++) {
+            int leftc = 0; int rightc = 0;
+            
+            if (i <= N - 3 && arr[i + 1] != arr[i] && arr[i + 2] != arr[i]) {
+                for (int j = i + 1; arr[j] != arr[i] && j < N - 1; j++) {
+                    rightc++;
+                }
             }
-            solve(pw, nums);
+            if (i >= 2 && arr[i - 1] != arr[i] && arr[i - 2] != arr[i]) {
+                for (int j = i - 1; arr[j] != arr[i] && j > 0; j--) {
+                    leftc++;
+                }
+            }
+            if (i >= 1 && i < N - 1 && arr[i + 1] != arr[i] && arr[i - 1] != arr[i]) {
+                for (int j = i + 1; arr[j] != arr[i] && j < N - 1; j++) {
+                    rightc++;
+                }
+                for (int j = i - 1; arr[j] != arr[i] && j > 0; j--) {
+                    leftc++;
+                }
+                count++;
+            }
+            count += (rightc > 0) ? rightc - 1 : 0;
+            count += (leftc > 0) ? leftc - 1 : 0;
         }
+        pw.println(count);
         pw.close();
     }
 }
