808791f09ffcd0edff453ea437669e4f58750996
froge <87875402+froge159@users.noreply.github.com>
2023-12-03 21:57:16 -0600

    Update temporaryFile.java

--- temporaryFile.java
+++ temporaryFile.java
@@ -1,7 +1,14 @@
 import java.util.*;
 import java.io.*;
-// http://www.usaco.org/index.php?page=viewproblem2&cpid=1155
-public class Photo {
+
+public class Air {
+    static int findMin(ArrayList<Integer> inds, int[] diff) {
+        int minval = Integer.MAX_VALUE;
+        for (int i: inds) {
+            if (Math.abs(diff[i]) < minval) minval = diff[i];
+        }
+        return minval;
+    }
     public static void main(String[] args) throws IOException {
         // BufferedReader br = new BufferedReader(new FileReader("input.in"));
         // PrintWriter pw = new PrintWriter("output.out");
@@ -9,39 +16,51 @@ public class Photo {
         PrintWriter pw = new PrintWriter(System.out);
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
+        int[] res = new int[N]; int[] start = new int[N]; int[] diff = new int[N];
         st = new StringTokenizer(br.readLine());
-        char[] arr = st.nextToken().toCharArray();
+        for (int i = 0; i < N; i++) res[i] = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) { 
+            start[i] = Integer.parseInt(st.nextToken());
+            diff[i] = res[i] - start[i];
+        }
         int count = 0;
+        ArrayList<Integer> inds = new ArrayList<>();
 
         for (int i = 0; i < N; i++) {
-            int leftc = 0; int rightc = 0;
-            
-            if (i >= 1 && i < N - 1 && arr[i + 1] != arr[i] && arr[i - 1] != arr[i]) {
-                for (int j = i + 1; j <= N - 1; j++) {
-                    if (arr[j] == arr[i]) break;
-                    rightc++;
+            if (diff[i] != 0) {
+                inds.add(i);
+                int j = i + 1;
+                while (true) {
+                    if ((diff[j - 1] < 0 && diff[j] < 0) || (diff[j - 1] > 0 && diff[j] > 0)) {
+                        inds.add(j);
+                        j++;
+                    }
+                    else {
+                        int min = findMin(inds, diff);
+                        for (int k = inds.get(0); k <= inds.get(inds.size() - 1); k++) {
+                            if (inds.get(0) < 0) {
+                                start[k] += min;
+                            }
+                            else start[k] -= min;
+                            diff[k] = res[k] - start[k];
+                        }
+                        count++;
+
+                        int prev = diff[inds.get(0)];
+
+                        for (int k = inds.get(1); k <= inds.get(inds.size() - 1); k++) {
+                            if (diff[inds.get(k)] != prev) {
+                                count++;
+                                prev = diff[inds.get(k)];
+                            }
+                        }
+                        inds.clear();
+                        break;
+                    }
                 }
-                for (int j = i - 1; j >= 0; j--) {
-                    if (arr[j] == arr[i]) break;
-                    leftc++;
-                }
-                count++;
+                i = j;
             }
-            else if (i <= N - 3 && arr[i + 1] != arr[i] && arr[i + 2] != arr[i]) {
-                for (int j = i + 1; j <= N - 1; j++) {
-                    if (arr[j] == arr[i]) break;
-                    rightc++;
-                }
-            }
-            else if (i >= 2 && arr[i - 1] != arr[i] && arr[i - 2] != arr[i]) {
-                for (int j = i - 1; j >= 0; j--) {
-                    if (arr[j] == arr[i]) break;
-                    leftc++;
-                }
-            }
-            count += (rightc > 0) ? rightc - 1 : 0;
-            count += (leftc > 0) ? leftc - 1 : 0;
-            //pw.println(leftc + " " + rightc);
         }
         pw.println(count);
         pw.close();
