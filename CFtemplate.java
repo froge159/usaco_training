582239661db0c583a54011ad543e399fd0752cb8
froge <87875402+froge159@users.noreply.github.com>
2024-02-06 10:29:21 -0600

    Update CFtemplate.java

--- CFtemplate.java
+++ CFtemplate.java
@@ -1,65 +1,67 @@
 import java.util.*;
 import java.io.*;
 
-public class A {
-    static void solve(int[][] arr, PrintWriter pw) {
+public class Solve {
+    static FastReader in = new FastReader();
+    static PrintWriter pw = new PrintWriter(System.out);
+    /*
+    notes
+    */
+    static void solve() { 
         
+
     }
-    public static void main(String[] args) throws IOException {
-        CP cp = new CP();
-        int T = cp.toInt(cp.readLine());
+    public static void main(String[] args) throws IOException {        
+        int T = in.nextInt();
         while (T-- > 0) {
-            
+            solve();
         }
+        pw.close();
     }
+    
 }
 
+class FastReader {
 
-class CP {
     BufferedReader br;
-    PrintWriter pw;
     StringTokenizer st;
-    // tokenizeLine, readLine, nextString, nextInt, nextLong, close, 
-    public CP() {
+
+    public FastReader() {
         br = new BufferedReader(new InputStreamReader(System.in));
-        pw = new PrintWriter(System.out);
-    }
-    public void tokenizeLine() throws IOException { 
-        st = new StringTokenizer(br.readLine());
-    }
-    public String readLine() throws IOException { 
-        return br.readLine();
-    }
-    public int toInt(String line) {
-        return Integer.parseInt(line);
     }
 
-
-    public String nextString() throws IOException { 
+    String next() {
+        while (st == null || !st.hasMoreElements()) {
+            try {
+                st = new StringTokenizer(br.readLine());
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
         return st.nextToken();
     }
-    public int nextInt() throws IOException { 
-        return Integer.parseInt(st.nextToken());
-    }
-    public long nextLong() throws IOException { 
-        return Long.parseLong(st.nextToken());
+
+    int nextInt() {
+        return Integer.parseInt(next());
     }
 
-    
-    public void println(char x) {pw.println(x);}
-    public void println(String x) {pw.println(x);}
-    public void println(int x) {pw.println(x);}
-    public void println(double x) {pw.println(x);}
-    public void println(long x) {pw.println(x);}
-    public void print(char x) {pw.print(x);}
-    public void print(String x) {pw.print(x);}
-    public void print(int x) {pw.print(x);}
-    public void print(double x) {pw.print(x);}
-    public void print(long x) {pw.print(x);}
-    public void println(StringBuilder sb) {pw.println(sb);}
-
-    public void close() throws IOException { // closes
-        br.close(); pw.close();
+    long nextLong() {
+        return Long.parseLong(next());
     }
 
+    double nextDouble() {
+        return Double.parseDouble(next());
+    }
+
+    String nextLine() {
+        String str = "";
+        try {
+            str = br.readLine();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        return str;
+    }
 }
